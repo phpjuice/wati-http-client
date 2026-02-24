42354bd9063f5e92e0d63eca4a4241888563d851
Mohammed Elhaouari <m@codiume.com>
2026-02-24 02:25:44 +0100

    WIP

--- src/WatiResponse.php
+++ src/WatiResponse.php
@@ -7,4 +7,26 @@ namespace Wati\Http;
 use GuzzleHttp\Psr7\Response;
 use Psr\Http\Message\ResponseInterface;
 
-abstract class WatiResponse extends Response implements ResponseInterface {}
+final class WatiResponse extends Response implements ResponseInterface
+{
+    public static function fromResponse(ResponseInterface $response): self
+    {
+        return new self(
+            $response->getStatusCode(),
+            $response->getHeaders(),
+            $response->getBody(),
+            $response->getProtocolVersion(),
+            $response->getReasonPhrase()
+        );
+    }
+    
+    public function json(): array
+    {
+        return json_decode($this->getBody()->getContents(), true);
+    }
+
+    public function isSuccessful(): bool
+    {
+        return ($this->json()['result'] ?? null) === 'success';
+    }
+}
